394718e491d473954ae3eb897f4a2e77f7f38fbc
Cloudisoft <vikram@cloudisoft.com>
2025-12-02 04:41:11 +0530

    Update Pricing.tsx

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -23,7 +23,7 @@ export default function PricingSection() {
           target="_blank"
           rel="noreferrer"
         > */}
-      <Link className="btn-primary inline-flex items-center gap-2" to="/cloudicore">
+      <Link className="btn-primary inline-flex items-center gap-2 mt-4 flex flex-col items-center justify-center gap-3 md:flex-row" to="/cloudicore">
           Start Your Free Trial
           <span>▶</span>
         </Link>
