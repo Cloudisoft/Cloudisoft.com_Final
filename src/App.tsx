06e1bfcda86cdc25fced6d211d26fc8d50bd1299
Cloudisoft <vikram@cloudisoft.com>
2025-12-02 03:02:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import AgentsSection from "./components/AgentsSection";
-import CloudiCoreSection from "./components/CloudiCoreSection";
-import Pricing from "./components/Pricing";
-import ContactSection from "./components/ContactSection";
-import Footer from "./components/Footer";
-import CloudiCore from "./pages/cloudicore"; // correct import
-
-function Home() {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-black via-cloudi-bg to-black text-white">
-      <Navbar />
-      <main className="pt-20 space-y-32 pb-24">
-        <Hero />
-        <AgentsSection />
-        <CloudiCoreSection />
-        <Pricing />
-        <ContactSection />
-      </main>
-      <Footer />
-    </div>
-  );
-}
+import Home from "./Home";
+import CloudiCore from "./pages/cloudicore";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cloudicore" element={<CloudiCore />} /> 
+        <Route path="/cloudicore" element={<CloudiCore />} />
       </Routes>
     </Router>
   );
