83af0c6c217ba2a9b3535936bcde272130f38171
Cloudisoft <vikram@cloudisoft.com>
2025-12-09 02:41:13 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 
 const Login: React.FC = () => {
-  const CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID"; // <-- paste your ID here
+  const CLIENT_ID =
+    "757924400568-0at91ohkm7hau66gm2od6lta14kc27tu.apps.googleusercontent.com";
 
-  const redirectToGoogle = () => {
-    const redirectURI = "https://cloudisoft.com/auth/callback"; // or localhost for testing
+  // While testing locally, switch redirect uri to localhost
+  const redirectURI = "https://cloudisoft.com/auth/callback";
+  // const redirectURI = "http://localhost:5173/auth/callback";  // for local testing
 
-    const authURL = 
+  const loginWithGoogle = () => {
+    const authURL =
       "https://accounts.google.com/o/oauth2/v2/auth" +
-      "?client_id=" + CLIENT_ID +
-      "&redirect_uri=" + redirectURI +
+      "?client_id=" +
+      CLIENT_ID +
+      "&redirect_uri=" +
+      redirectURI +
       "&response_type=code" +
-      "&scope=openid%20profile%20email";
+      "&scope=openid%20email%20profile";
 
-    window.location.href = authURL; // send user to Google login
+    window.location.href = authURL;
   };
 
   return (
-    <div style={{ padding: "30px" }}>
-      <h2>Login to CloudiCore</h2>
-      <button onClick={redirectToGoogle}>Login with Google</button>
+    <div className="min-h-screen flex justify-center items-center bg-black text-white">
+      <button
+        onClick={loginWithGoogle}
+        className="px-6 py-3 bg-blue-500 rounded-lg text-lg font-semibold hover:bg-blue-600"
+      >
+        Login with Google
+      </button>
     </div>
   );
 };
